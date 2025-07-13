57bb16954ecc25a198966227414b4bb7161db404
Aravindmu <146928881+Aravindmu@users.noreply.github.com>
2025-07-13 08:37:30 +0530

    Delete cart directory

--- cart/customers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.conf.urls.static import static
-from . import views
-
-urlpatterns = [
-    path('account',views.show_account, name='account'), 
-    path('signout', views.signout, name='signout'),
-]
