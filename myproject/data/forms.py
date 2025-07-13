3db9c1c8d64fdef10f082858c8e4d4457ac0a55d
Aravindmu <146928881+Aravindmu@users.noreply.github.com>
2025-07-13 08:26:57 +0530

    Delete myproject directory

--- myproject/data/forms.py
+++
@@ -1,8 +0,0 @@
-# data/forms.py
-from django import forms
-from .models import UserData
-
-class UserDataForm(forms.ModelForm):
-    class Meta:
-        model = UserData
-        fields = ['username_field', 'phone_number', 'media_file'] # Add media_file
