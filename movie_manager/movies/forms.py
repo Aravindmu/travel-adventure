f271c6cd6bc5492777276bb7e7d7e4f7010de4bd
Aravindmu <146928881+Aravindmu@users.noreply.github.com>
2025-07-13 08:26:46 +0530

    Delete movie_manager directory

--- movie_manager/movies/forms.py
+++
@@ -1,6 +0,0 @@
-from django.forms import ModelForm
-from . models import MovieInformation
-class MovieForm(ModelForm):
-    class Meta: ##Meta is using for description the main class 
-        model=MovieInformation
-        fields='__all__'
